999958a8dc3520dcacbc05426fea8b3c89091a77
Zekken01 <brodycas3@gmail.com>
2024-02-13 21:04:45 -0500

    .

--- js/browser.js
+++ js/browser.js
@@ -22,7 +22,7 @@ var controllersOn = false;
 var VerifyIPRegex = /^(?:(?:2[0-4]\d|25[0-5]|1\d{2}|[1-9]?\d)\.){3}(?:2[0-4]\d|25[0-5]|1\d{2}|[1-9]?\d)(?:\:(?:\d|[1-9]\d{1,3}|[1-5]\d{4}|6[0-4]\d{3}|65[0-4]\d{2}|655[0-2]\d|6553[0-5]))?$/;
 
 var dewritoURLList = [
-    "https://raw.githubusercontent.com/Zekken01/Scooterpsu-Menu/master/dewrito.json",
+    "https://raw.githubusercontent.com/Zekken01/Scooterpsu-Menu/master/dewrito.json"
 ];
 var URLIndex = 0;
 var dewritoURL = dewritoURLList[URLIndex];

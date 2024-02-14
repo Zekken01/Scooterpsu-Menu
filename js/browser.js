3bf8b604d62878b9091267f9f63c5d9648d1c84c
Zekken01 <brodycas3@gmail.com>
2024-02-13 22:42:02 -0500

    HTTPS be gone

--- js/browser.js
+++ js/browser.js
@@ -22,8 +22,8 @@ var controllersOn = false;
 var VerifyIPRegex = /^(?:(?:2[0-4]\d|25[0-5]|1\d{2}|[1-9]?\d)\.){3}(?:2[0-4]\d|25[0-5]|1\d{2}|[1-9]?\d)(?:\:(?:\d|[1-9]\d{1,3}|[1-5]\d{4}|6[0-4]\d{3}|65[0-4]\d{2}|655[0-2]\d|6553[0-5]))?$/;
 
 var dewritoURLList = [
-    	"https://raw.githubusercontent.com/Zekken01/Scooterpsu-Menu/master/dewrito.json",
-	"https://gist.githubusercontent.com/Zekken01/9d9183fa761b8a88c109adf6654c8652/raw/a94b74972055c82f51ae6d35313ae5dd470e5643/dewrito.json"
+	"http://raw.githubusercontent.com/Zekken01/Scooterpsu-Menu/master/dewrito.json",
+	"http://gist.githubusercontent.com/Zekken01/9d9183fa761b8a88c109adf6654c8652/raw/a94b74972055c82f51ae6d35313ae5dd470e5643/dewrito.json"
 ];
 var URLIndex = 0;
 var dewritoURL = dewritoURLList[URLIndex];
